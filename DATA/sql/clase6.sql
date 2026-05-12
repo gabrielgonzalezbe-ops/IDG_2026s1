0e6fd801d7ef929614752e5abcd3f8207cd0d97d
gabrielgonzalezbe-ops <gabriel.gonzalez.be@usach.cl>
2026-05-12 09:48:33 -0400

    se VIO QGIS

--- DATA/sql/clase6.sql
+++ DATA/sql/clase6.sql
@@ -35,3 +35,24 @@ ON t.geocodigo::double precision = v.geocodigo
 -- PARA SABER EN QUÉ SISTEMA DE COORDENAS ESTÁ LO CREADO
 SELECT ST_STRID(geom)
 FROM tot_prof_geom
+
+
+
+
+
+
+-- PORCENTAJE DE VIVIENDAS HACINADAS POR ZONA CENSAL
+SELECT *
+FROM "variables_codificacion" -- ind_hacin_rec
+
+
+CREATE TABLE output.viv_hacinadas AS
+SELECT c.nom_comuna, z.geocodigo, COUNT(*) AS hacinamiento
+
+FROM public.viviendas AS v
+JOIN public.zonas AS z
+ON v.zonaloc_ref_id = z.zonaloc_ref_id
+JOIN public.comunas AS c
+ON z.codigo_comuna = c.codigo_comuna
+
+GROUP BY z.geocodigo, c.nom_comuna
