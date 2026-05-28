06cfa7757d45642db77a7e95b8194e40a28a4684
gabrielgonzalezbe-ops <gabriel.gonzalez.be@usach.cl>
2026-05-28 11:03:27 -0400

    se realizaron códigos en Jupyter

--- DATA/sql/clase6.sql
+++ DATA/sql/clase6.sql
@@ -74,21 +74,18 @@ FROM personas AS p
 JOIN hogares AS h
 ON p.hogar_ref_id =h.hogar_ref_id
 JOIN viviendas AS v
-ON h.vivienda_ref_id =v.vivienda_ref_id
+ON h.vivienda_ref_ind =v.vivienda_ref_ind
 JOIN zonas AS z
 ON v.zonaloc_ref_id = z.zonaloc_ref_id
 JOIN comunas AS c
 ON z.codigo_comuna = c.codigo_comuna
-GROUP BY z.geocodigo, c.nom_comuna
+GROUP BY z.geodigo, c.nom.comuna
 ORDER BY porcentaje DESC
 
 CREATE TABLE output.porcentaje_hacinamiento_geom AS
 (
 SELECT shp.geom, tabla.*
-FROM output.porcentaje_hacinamiento AS tabla
+FROM output.oircetaje_hacinamiento AS tabla
 JOIN dpa.zonas_censales_v AS shp
-ON tabla.geocodigo::DOUBLE PRECISION = shp.geocodigo);
-
-
-
-
+ON tabla.geocodigo::DOUBLE PRECISION = shp.zonas_censal
+)
